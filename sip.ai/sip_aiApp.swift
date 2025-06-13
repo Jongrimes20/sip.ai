cee9f19c398610be5c9a794a83b09130bc9717d4
Jon Grimes <jongrimes20@gmail.com>
2025-06-13 18:04:35 -0500

    Initial Commit

---
+++ sip.ai/sip_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sip_aiApp.swift
+//  sip.ai
+//
+//  Created by Jon Grimes on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct sip_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
